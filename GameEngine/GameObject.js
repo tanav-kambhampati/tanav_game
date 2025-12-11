9b038d74fad10532be82d4f344570aecc125071d
Tanav Kambhampati <fifteentanav@gmail.com>
2025-12-11 14:23:16 -0800

    gameobject init

---
+++ GameEngine/GameObject.js
@@ -0,0 +1,10 @@
+@class GameObject
+@method draw 
+@method update 
+@method resize
+@method destroy 
+@method collisionChecks 
+@method isCollision 
+@method handleCollisionEvent 
+@method handleReaction 
+
