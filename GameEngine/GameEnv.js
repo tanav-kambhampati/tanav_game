5c4c778b6d65d746fb11f9bebf3ff7dd20772fff
Tanav Kambhampati <fifteentanav@gmail.com>
2025-12-11 13:31:02 -0800

    gameEnv file

--- GameEngine/GameEnv.js
+++ GameEngine/GameEnv.js
@@ -1,6 +1,6 @@
 // This file is going to manage the game environment
 
-@class
+@class GameEnv
 @property {Object} container
 @property {Object} canvas 
 @property {Object} ctx //2D rendering width of canvas
@@ -39,11 +39,30 @@ class GameEnv {
         this.canvas = document.getElementById('gameCanvas');
         this.ctx = this.canvas.getContext('2d', { willReadFrequently: true });
     }
-
     setTop() {
     const header = document.querySelector('header');
     this.top = header ? header.offsetHeight : 0;
     }
+    setBottom() {
+        const footer = document.querySelector('footer');
+        this.bottom = footer ? footer.offsetHeight : 0;
+    }
 
+    size() {
+        this.canvas.width = this.innerWidth;
+        this.canvas.height = this.innerHeight;
+        this.canvas.style.width = `${this.innerWidth}px`;
+        this.canvas.style.height = `${this.innerHeight}px`;
+        this.canvas.style.position = 'absolute';
+        this.canvas.style.left = '0px';
+        this.canvas.style.top = `${this.top}px`;
+    }
+
+    resize() {
+        this.create();
+    }
+
+     clear() {
+        this.ctx.clearRect(0, 0, this.innerWidth, this.innerHeight);
+    }
 }
-
