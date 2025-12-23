cda70bcaba96335f680e10abe2eade05abe50a8b
Tanav Kambhampati <fifteentanav@gmail.com>
2025-12-23 08:31:14 -0800

    gamelevelhistory

---
+++ GameLevelHistory.js
@@ -0,0 +1,60 @@
+import GameEnvBackground from './GameEngine/GameEnvBackground.js';
+import Player from './GameEngine/Player.js';
+
+
+class GameLevelHistory {
+  constructor(gameEnv) {
+    this.gameEnv = gameEnv;
+    this.width = gameEnv.innerWidth;
+    this.height = gameEnv.innerHeight;
+    this.path = gameEnv.path;
+
+    this.classes = this.buildLevel();
+  }
+
+  createBackgroundData() {
+    return {
+      name: 'history_archives',
+      greeting: "Welcome to the History Archives!",
+      src: this.path + "/images/gamify/spacebase.png",
+      pixels: { height: 966, width: 654 }
+    };
+  }
+
+  createPlayerData() {
+    const scaleFactor = 5;
+    return {
+      id: 'Settler',
+      greeting: "Time to learn from the pioneers!",
+      src: this.path + "/images/gamify/chillguy.png",
+      SCALE_FACTOR: scaleFactor,
+      STEP_FACTOR: 1000,
+      ANIMATION_RATE: 50,
+      INIT_POSITION: { x: 0, y: this.height - (this.height / scaleFactor) },
+      pixels: { height: 384, width: 512 },
+      orientation: { rows: 3, columns: 4 },
+      down: { row: 0, start: 0, columns: 3 },
+      left: { row: 2, start: 0, columns: 3 },
+      right: { row: 1, start: 0, columns: 3 },
+      up: { row: 3, start: 0, columns: 3 },
+      hitbox: { widthPercentage: 0.45, heightPercentage: 0.2 },
+      keypress: { up: 87, left: 65, down: 83, right: 68 }
+    };
+  }
+
+  buildLevel() {
+    return [
+      { class: GameEnvBackground, data: this.createBackgroundData() },
+      { class: Player, data: this.createPlayerData() },
+    ];
+  }
+
+  completeLevel() {
+    if (this.gameEnv.gameControl) {
+      this.gameEnv.gameControl.gameOver();
+    }
+  }
+}
+
+export default GameLevelHistory;
+
