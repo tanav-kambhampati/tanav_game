cce2669a1e7b2e613918bea89af760b61f2180ef
Tanav Kambhampati <fifteentanav@gmail.com>
2025-12-26 17:11:01 -0800

    env changes for modular scaling

--- GameEngine/GameEnv.js
+++ GameEngine/GameEnv.js
@@ -1,17 +1,17 @@
-// This file is going to manage the game environment
+/**
 
-@class GameEnv
-@property {Object} container
-@property {Object} canvas 
-@property {Object} ctx //2D rendering width of canvas
-@property {number} innerWidth
-@property {number} innerHeight .
-@property {number} top
-@property {number} bottom
-
+ * @class GameEnv
+ * @property {Object} container
+ * @property {Object} canvas 
+ * @property {Object} ctx 
+ * @property {number} innerWidth 
+ * @property {number} innerHeight 
+ * @property {number} top
+ * @property {number} bottom 
+ */
 class GameEnv {
-    constructor(){
-        this.container = null
+    constructor() {
+        this.container = null;
         this.canvas = null;
         this.ctx = null;
         this.innerWidth = 0;
@@ -22,27 +22,30 @@ class GameEnv {
         this.game = null; 
         this.path = ''; 
         this.gameControl = null; 
-        this.gameObjects = [];
-
+        this.gameObjects = [];    
     }
-    
+
+     
     create() {
-    this.setCanvas();
-    this.setTop();
-    this.setBottom();
-    this.innerWidth = window.innerWidth;
-    this.innerHeight = window.innerHeight - this.top - this.bottom;
-    this.size();
-    }   
+        this.setCanvas();
+        this.setTop();
+        this.setBottom();
+        this.innerWidth = window.innerWidth;
+        this.innerHeight = window.innerHeight - this.top - this.bottom;
+        this.size();
+    }
+
 
     setCanvas() {
         this.canvas = document.getElementById('gameCanvas');
         this.ctx = this.canvas.getContext('2d', { willReadFrequently: true });
     }
+
     setTop() {
-    const header = document.querySelector('header');
-    this.top = header ? header.offsetHeight : 0;
+        const header = document.querySelector('header');
+        this.top = header ? header.offsetHeight : 0;
     }
+
     setBottom() {
         const footer = document.querySelector('footer');
         this.bottom = footer ? footer.offsetHeight : 0;
@@ -58,11 +61,15 @@ class GameEnv {
         this.canvas.style.top = `${this.top}px`;
     }
 
+
     resize() {
         this.create();
     }
 
-     clear() {
+ 
+    clear() {
         this.ctx.clearRect(0, 0, this.innerWidth, this.innerHeight);
     }
 }
+
+export default GameEnv;
